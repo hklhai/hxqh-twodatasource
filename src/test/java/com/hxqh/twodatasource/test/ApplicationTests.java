4619bb8f079f1f21876f93aa2e340d5b36eb2c6a
hklhai <hkhai@outlook.com>
2017-07-14 15:57:44 +0700

    test remove

--- src/test/java/com/hxqh/twodatasource/test/ApplicationTests.java
+++ src/test/java/com/hxqh/twodatasource/test/ApplicationTests.java
@@ -21,74 +21,74 @@ public class ApplicationTests {
     private SystemService systemService;
 
 
-    @Test
-    public void testTAlertEnv() throws Exception {
-        systemService.saveAlertEnvs();
-    }
-
-    @Test
-    public void testProactive() throws Exception {
-        systemService.saveTtwifiMonitor();
-    }
-
-    @Test
-    public void testSaveTLvlEnterpriseCust() throws Exception {
-        systemService.saveEnterpriseCusts();
-    }
-
-    @Test
-    public void testTPortdown4ioc() throws Exception {
-        systemService.saveTPortdown();
-    }
-
-    @Test
-    public void testTStoKoordinat() throws Exception {
-        systemService.saveTStoKoordinat();
-    }
-
-    @Test
-    public void testTtwifiMttr() throws Exception {
-        systemService.saveTtwifiMttr();
-    }
-
-    @Test
-    public void testTPerfEnterprise4tiocRepository() throws Exception {
-        systemService.saveTPerfEnterprise4tiocRepository();
-    }
-
-
-    //调用存储过程  analysis_source_ent_4tioc1
-    //V_PERF_ENTERPRISE_4TIOC1           tb_ioctperfenterprise4tioc      Analysis_Source_ENT_4TIOC1
-    @Test
-    public void testIoctperfenterprise4tioc() throws Exception {
-        systemService.analysis_source_ent_4tioc1();
-    }
-
-    //调用存储过程  analysis_data_event_altenv
-    //t_alert_env             tb_iocenterpriseevent       Analysis_Data_Event_Altenv
-    @Test
-    public void testIocenterpriseevent() throws Exception {
-        systemService.analysis_data_event_altenv();
-    }
-
-    //调用存储过程  analysis_source_portdown
-    //T_PORTDOWN_4IOC             tb_IOCTPORTDOWN4IOC           Analysis_Source_PORTDOWN
-    @Test
-    public void testPortdown() throws Exception {
-        systemService.analysis_source_portdown();
-    }
-
-    //ttwifi_monitor_mttr_proactive        tb_IOCTICKETSCREEN100       Analysis_Data_Mttr_Proactive
-    @Test
-    public void testIocticketscreen100() throws Exception {
-        systemService.analysis_data_mttr_proactive();
-    }
-
-    //ttwifi_mttr_proactive_last_month     tb_iocticketScreen96        Analysis_Data_Mttr_Targets
-    @Test
-    public void testIocticketScreen96  () throws Exception {
-        systemService.analysis_data_mttr_targets();
-    }
+//    @Test
+//    public void testTAlertEnv() throws Exception {
+//        systemService.saveAlertEnvs();
+//    }
+//
+//    @Test
+//    public void testProactive() throws Exception {
+//        systemService.saveTtwifiMonitor();
+//    }
+//
+//    @Test
+//    public void testSaveTLvlEnterpriseCust() throws Exception {
+//        systemService.saveEnterpriseCusts();
+//    }
+//
+//    @Test
+//    public void testTPortdown4ioc() throws Exception {
+//        systemService.saveTPortdown();
+//    }
+//
+//    @Test
+//    public void testTStoKoordinat() throws Exception {
+//        systemService.saveTStoKoordinat();
+//    }
+//
+//    @Test
+//    public void testTtwifiMttr() throws Exception {
+//        systemService.saveTtwifiMttr();
+//    }
+//
+//    @Test
+//    public void testTPerfEnterprise4tiocRepository() throws Exception {
+//        systemService.saveTPerfEnterprise4tiocRepository();
+//    }
+//
+//
+//    //调用存储过程  analysis_source_ent_4tioc1
+//    //V_PERF_ENTERPRISE_4TIOC1           tb_ioctperfenterprise4tioc      Analysis_Source_ENT_4TIOC1
+//    @Test
+//    public void testIoctperfenterprise4tioc() throws Exception {
+//        systemService.analysis_source_ent_4tioc1();
+//    }
+//
+//    //调用存储过程  analysis_data_event_altenv
+//    //t_alert_env             tb_iocenterpriseevent       Analysis_Data_Event_Altenv
+//    @Test
+//    public void testIocenterpriseevent() throws Exception {
+//        systemService.analysis_data_event_altenv();
+//    }
+//
+//    //调用存储过程  analysis_source_portdown
+//    //T_PORTDOWN_4IOC             tb_IOCTPORTDOWN4IOC           Analysis_Source_PORTDOWN
+//    @Test
+//    public void testPortdown() throws Exception {
+//        systemService.analysis_source_portdown();
+//    }
+//
+//    //ttwifi_monitor_mttr_proactive        tb_IOCTICKETSCREEN100       Analysis_Data_Mttr_Proactive
+//    @Test
+//    public void testIocticketscreen100() throws Exception {
+//        systemService.analysis_data_mttr_proactive();
+//    }
+//
+//    //ttwifi_mttr_proactive_last_month     tb_iocticketScreen96        Analysis_Data_Mttr_Targets
+//    @Test
+//    public void testIocticketScreen96  () throws Exception {
+//        systemService.analysis_data_mttr_targets();
+//    }
 
 }
 
